89137926d220a5bd7c9d5bb150ce91be2121f2b9
Daniel Ran <danielr1735@gmail.com>
2018-06-20 19:34:32 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest = nil
   name_hash.each do |key, value|
-    if !lowest || value < lowest
+    if lowest == nil || value < lowest
       lowest = key
     end
   end
